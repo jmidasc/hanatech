531b56461ef8c208d522de0486dcad9f064cb2ec
DESKTOP-H3I9Q76\jon <terryzhou@gmail.com>
2022-04-18 08:24:56 -0700

    cluth1

--- src/components/CustomerSaying/clutch.js
+++ src/components/CustomerSaying/clutch.js
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
-
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons'
 import { Carousel } from 'react-responsive-carousel'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 import './clutch.css';
+
+const carouselProp = {
+  showStatus: false,
+  infiniteLoop: true,
+  renderArrowPrev: (clickHandler, hasPrev, label) => {
+    return (
+      <div className="arrow-left" onClick={clickHandler}>
+        <FontAwesomeIcon icon={faAngleLeft}></FontAwesomeIcon>
+      </div>
+    )
+  },
+  renderArrowNext: (clickHandler, hasNext, label) => {
+    return (
+      <div className="arrow-right" onClick={clickHandler}>
+        <FontAwesomeIcon icon={faAngleRight}></FontAwesomeIcon>
+      </div>
+    )
+  },
+}
+
 export default () => {
 
   return (
     <div className="clutch">
       <div className='clutch-header'>Clutch</div>
       <div className='clutch-content'>
-        <div className='clutch-subheader'>HanaTech</div>
+        <div className='clutch-subheader'>Flyoutsourcing</div>
         <div className='clutch-formatter'>
           <div className='star-container'>
             <div className='star-one'>
@@ -33,13 +53,11 @@ export default () => {
           <div className='review-header'>3 REVIEWS</div>
         </div>
         <div className='clutch-subcontainer'>
-          <Carousel
-            infiniteLoop
-            showStatus={false}
+          <Carousel {...carouselProp}
             >
             <div className='text-container'>
               <div className='text-content'>
-                "HanaTech truly had some of the most professional people I’ve worked with."
+                "Flyoutsourcing truly had some of the most professional people I’ve worked with."
               </div>
               <div className='text-author'>
                 Project Manager, Publishing Company
@@ -47,7 +65,7 @@ export default () => {
             </div>
             <div className='text-container'>
               <div className='text-content'>
-                "HanaTech truly had some of the most professional people I’ve worked with."
+                "Flyoutsourcing truly had some of the most professional people I’ve worked with."
               </div>
               <div className='text-author'>
                 Project Manager, Publishing Company
@@ -55,7 +73,7 @@ export default () => {
             </div>
             <div className='text-container'>
               <div className='text-content'>
-                "HanaTech truly had some of the most professional people I’ve worked with."
+                "Flyoutsourcing truly had some of the most professional people I’ve worked with."
               </div>
               <div className='text-author'>
                 Project Manager, Publishing Company
